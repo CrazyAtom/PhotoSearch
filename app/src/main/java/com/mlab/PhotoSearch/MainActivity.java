7233bb107212cd510cbe39dd50bd6d13378a8ef3
crazyatom <crazyatom84@gmail.com>
2016-04-01 13:46:05 +0900

    error resolved

--- app/src/main/java/com/mlab/PhotoSearch/MainActivity.java
+++ app/src/main/java/com/mlab/PhotoSearch/MainActivity.java
@@ -3,6 +3,7 @@ package com.mlab.PhotoSearch;
 import java.util.ArrayList;
 import android.content.Intent;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.content.res.TypedArray;
 import android.graphics.Bitmap;
@@ -32,12 +33,12 @@ public class MainActivity extends ActionBarActivity {
                 ImageItem item = (ImageItem) parent.getItemAtPosition(position);
 
                 //Create intent
-                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
-                intent.putExtra("title", item.getTitle());
-                intent.putExtra("image", item.getImage());
-
-                //Start details activity
-                startActivity(intent);
+//                Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
+//                intent.putExtra("title", item.getTitle());
+//                intent.putExtra("image", item.getImage());
+//
+//                //Start details activity
+//                startActivity(intent);
             }
         });
     }
@@ -47,10 +48,10 @@ public class MainActivity extends ActionBarActivity {
      */
     private ArrayList<ImageItem> getData() {
         final ArrayList<ImageItem> imageItems = new ArrayList<>();
-        ArrayList<String> thumbsIDs;
+        ArrayList<String> thumbsIDs = new ArrayList<String>();
         getThumbInfo(thumbsIDs);
 
-        for(itn i = 0; i < thumbsIDs.size(); i++) {
+        for(int i = 0; i < thumbsIDs.size(); i++) {
             imageItems.add(new ImageItem(thumbsIDs.get(i), "Image#" + i, ""));
         }
 
@@ -59,7 +60,7 @@ public class MainActivity extends ActionBarActivity {
 //        for (int i = 0; i < imgs.length(); i++) {
 //            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
 //            imageItems.add(new ImageItem(bitmap, "Image#" + i));
-        }
+//        }
         return imageItems;
     }
 
@@ -67,24 +68,28 @@ public class MainActivity extends ActionBarActivity {
      * 앨범 사진들에 대한 썸네일 이미지 정보 수집
      */
     private void getThumbInfo(ArrayList<String> thumbsIDs) {
-        String[] projection = { MediaStore.Images.Thumbnails._ID };
-        Cursor imageCursor = managedQuery(MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI,
-                projection, null, null, MediaStore.Images.Thumbnails.IMAGE_ID + "");
+//        final String[] columns = { MediaStore.Images.Media.DATA, MediaStore.Images.Media._ID };
+//        final String orderBy = MediaStore.Images.Media._ID;
+//        final Uri uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        final String[] columns = { MediaStore.Images.Thumbnails.DATA, MediaStore.Images.Thumbnails._ID };
+        final String orderBy = MediaStore.Images.Thumbnails._ID;
+        final Uri uri = MediaStore.Images.Thumbnails.EXTERNAL_CONTENT_URI;
+        Cursor imagecursor = getContentResolver().query(uri, columns, null, null, orderBy);
 
-        if(imageCursor != null && imageCursor.moveToFirst()) {
+        if(imagecursor != null && imagecursor.moveToFirst()) {
             String date;
             String thumbsID;
-            int imageColumIndex = imageCursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
+            int imageColumIndex = imagecursor.getColumnIndexOrThrow(MediaStore.Images.Thumbnails._ID);
             do {
-                thumbsID = imageCursor.getString(imageColumIndex);
+                thumbsID = imagecursor.getString(imageColumIndex);
                 if(thumbsID.isEmpty() != true) {
                     thumbsIDs.add(thumbsID);
 //                    thumbsDates.add(date);
                 }
-            } while (imageCursor.moveToNext())
+            } while (imagecursor.moveToNext());
         }
 
-        imageCursor.close();
+        imagecursor.close();
 
         return;
     }
